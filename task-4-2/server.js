501cb7dc5957de9e79a6ff670848f3ad6817f054
Robin Bühler <git+r@obin.ch>
2024-04-16 11:43:12 +0000

    chore: task 4 2

--- task-4-2/server.js
+++ task-4-2/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 
 app.get('/now', (request, response) => {
   response.send(new Date().toLocaleTimeString("de-CH", {timeZone: request.query.tz}));
-
 });
+
+const names = [
+  "Franz",
+  "Nadine",
+  "Julia",
+  "Geroge"
+]
+
+app.post('/names', (req, res) => {
+  names.push(req.body.name);
+  res.send(names)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
